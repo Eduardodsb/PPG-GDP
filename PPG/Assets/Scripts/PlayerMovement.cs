c1c5d2765e6922f1a0661589989550c4418e6b89
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-08-12 17:58:02 -0300

    +Camera, Player, PlayerMovement

---
+++ PPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    GameObject cam;
+    GameObject oldCam = null;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.CompareTag("Cam")){
+
+            if (cam != null){
+                //Desativar Câmera atual/velha
+                cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 0;
+            }
+
+            cam = GameObject.Find("CMv" + collision.name); //Obtém o nome da nova câmera
+            cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 1; //Ativa a nova câmera
+        }
+
+    }
+
+}
