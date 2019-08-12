90d16ee9e6970463a39d81f941dcc82da0930a3d
Rafael Cardoso <cardoso.rf@gmail.com>
2019-08-12 20:18:04 -0300

    +Player script, PlayerData script, SaveSystem script; #PlayerMoviment script
    Criado as scripts que vão permitir salvar e carregar dados referentes ao personagem.
    
    Ajustado o script do PlayerMoviment para conter somente funções referentes a movimentação do personagem.

---
+++ PPG/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Player : MonoBehaviour
+{
+
+    GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Cam"))
+        {
+
+            if (cam != null)
+            {
+                //Desativar Câmera atual/velha
+                cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 0;
+            }
+
+            cam = GameObject.Find("CMv" + collision.name); //Obtém o nome da nova câmera
+            cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 1; //Ativa a nova câmera
+        }
+  
+    }
+
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+
+}
--- PPG/Assets/Scripts/PlayerMovement.cs
+++ PPG/Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
+
 
 public class PlayerMovement : MonoBehaviour {
-
-
-    GameObject cam;
-    GameObject oldCam = null;
 
     // Start is called before the first frame update
     void Start(){
@@ -17,21 +13,6 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update(){
         
-    }
-
-
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.CompareTag("Cam")){
-
-            if (cam != null){
-                //Desativar Câmera atual/velha
-                cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 0;
-            }
-
-            cam = GameObject.Find("CMv" + collision.name); //Obtém o nome da nova câmera
-            cam.GetComponent<CinemachineVirtualCamera>().m_Priority = 1; //Ativa a nova câmera
-        }
-
     }
 
 }
