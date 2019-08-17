c6f993bc90031e786f52a2ed2e661aabc02b3b1b
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-08-17 20:05:15 -0300

    #CamTriggers, CM ClearShot1

---
+++ PPG/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    bool isGamePaused = false;
+
+    public GameObject PauseMenu;
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (isGamePaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+        
+
+    }
+
+    private void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
