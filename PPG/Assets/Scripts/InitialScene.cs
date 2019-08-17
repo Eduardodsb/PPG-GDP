6348a8f943e7c771f3fea1d331faa54b1393b714
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-08-16 21:03:51 -0300

    #MenuScene

--- PPG/Assets/Scripts/InitialScene.cs
+++ PPG/Assets/Scripts/InitialScene.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InitialScene : MonoBehaviour{
+public class InitialScene : MonoBehaviour
+{
 
     GameObject text1;
     GameObject text2;
     GameObject text3;
     GameObject button;
 
-    void Start(){
+    GameObject soundManager;
+    bool keyboardSound = false;
+
+    void Start()
+    {
         text1 = GameObject.Find("Text1");
         text2 = GameObject.Find("Text2");
         text3 = GameObject.Find("Text3");
         button = GameObject.Find("EatFrog");
+        soundManager = GameObject.Find("SoundManager");
     }
 
-    public void NextScene(){
+    public void NextScene()
+    {
         text1.SetActive(false);
         text2.SetActive(false);
         text3.SetActive(false);
         button.SetActive(false);
-        gameObject.GetComponent<Animator>().SetBool("NextScene",true);
+        gameObject.GetComponent<Animator>().SetBool("NextScene", true);
     }
+
+    public void KeyboardSound(){
+        if (keyboardSound){
+            
+        }
+    }
+
 }
