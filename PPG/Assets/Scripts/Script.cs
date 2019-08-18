f06bffd6b60d1061c65725c9a37c0bc5b46bf740
Eduardo Barbosa <33110840+Eduardodsb@users.noreply.github.com>
2019-08-18 19:08:41 -0300

    +InkStory, #Drak

--- PPG/Assets/Scripts/Script.cs
+++ PPG/Assets/Scripts/Script.cs
@@ -89,16 +89,16 @@ public class Script : MonoBehaviour
 
 
     // Load and potentially return the next story block
-    string getNextStoryBlock()
-    {
+    string getNextStoryBlock(){
         string text = "";
         //text += story.Continue();
-        while (story.canContinue)
-        {
-            text += story.Continue();
-        }
-        
-        return text;
+          while (story.canContinue){
+              text += story.Continue();
+          }
+    /*    Debug.Log("oie");
+      story.EvaluateFunction("sala_central", "dudu");
+        Debug.Log(story.variablesState["name"]);
+        return text;*/
     }
 
     // Update is called once per frame
