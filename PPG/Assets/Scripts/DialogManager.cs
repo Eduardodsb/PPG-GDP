a0f0ec841daca669870bf890b4cd4a63e490cca0
Rafael Cardoso <cardoso.rf@gmail.com>
2019-08-29 22:40:22 -0300

    +DialogManager
    Adicionado o script para rodar diálogos ativado por trigger.

---
+++ PPG/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Dialogs
+{
+    public string name;
+    public GameObject objectDialog;
+}
+
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField]
+    public Dialogs[] dialogs;
+
+
+    Dictionary<int, string> dialogMap = new Dictionary<int, string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+
+    }
+
+}
